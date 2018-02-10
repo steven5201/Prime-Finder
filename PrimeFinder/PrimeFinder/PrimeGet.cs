973819afa85d3e694be754112ba577a3181f78ed
Steven Endres <steven520111@gmail.com>
2018-02-10 00:10:33 -0500

    Uses CPU Properly

---
+++ PrimeFinder/PrimeFinder/PrimeGet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFinder
+{
+    class PrimeGet
+    {
+        double startNum;
+        double endNum;
+        int taskNum;
+
+        List<double> primes = new List<double>();
+
+        public PrimeGet(double startNum, double endNum, int taskNum)
+        {
+            this.startNum = startNum;
+            this.endNum = endNum;
+            this.taskNum = taskNum;
+        }
+
+        public List<double> GetPrimes()
+        {
+            return primes;
+        }
+
+        public void NumsRun()
+        {
+            Console.WriteLine($"Task #{taskNum} running numbers {startNum} to {endNum}.");
+
+            for (double i = startNum; i < endNum; i++)
+            {
+                if (CheckPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+
+            Console.WriteLine($"Finished task #{taskNum}");
+        }
+
+        private bool CheckPrime(double inputNumber)
+        {
+            bool isPrime = true;
+
+            for (double lcv = 2; lcv < inputNumber - 1; lcv++)
+            {
+                if (inputNumber % lcv == 0)
+                {
+                    isPrime = false;
+                    lcv = inputNumber;
+                    break;
+                }
+            }
+
+            return isPrime;
+        }
+    }
+}
---
+++ PrimeFinder/PrimeFinder/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace PrimeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterationNum = GetBaseValues("How many tasks would you like to run: ");
+            int numChange = GetBaseValues("How many values per task would you like to run: ");
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the number you wish to use on the output file.");
+            Console.WriteLine("If the file already exists it will be added onto the end.");
+            Console.Write("Name: ");
+            string fileName = Console.ReadLine();
+
+            double currentNum = 0;
+            Task[] tasks = new Task[iterationNum];
+            PrimeGet[] primeGets = new PrimeGet[iterationNum];
+
+            for (int i = 0; i < iterationNum; i++)
+            {
+                currentNum += numChange;
+                primeGets[i] = new PrimeGet(currentNum - numChange, currentNum, i);
+                tasks[i] = Task.Run(new Action(primeGets[i].NumsRun));
+            }
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Writing primes to file...");
+
+            using (StreamWriter writer = File.AppendText($"{fileName}.Primes"))
+            {
+                List<double> primes = new List<double>();
+
+                foreach (PrimeGet primeGet in primeGets)
+                {
+                    primes.AddRange(primeGet.GetPrimes());
+                }
+
+                foreach (int num in primes)
+                {
+                    writer.WriteLine(num);
+                }
+            }
+        }
+
+        private static int GetBaseValues(string outputLine)
+        {
+            bool inputValid = true;
+            int num = 0;
+
+            do
+            {
+                Console.Write(outputLine);
+
+                inputValid = int.TryParse(Console.ReadLine(), out num);
+
+                if (inputValid)
+                {
+                    inputValid = (num > 0) ? true : false;
+                }
+
+                if (!inputValid)
+                {
+                    Console.WriteLine("Only positive whole numbers.");
+                }
+            } while (!inputValid);
+
+            return num;
+        }
+    }
+}
